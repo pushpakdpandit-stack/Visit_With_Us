2a137e221f18b07e2df30be3cd092391fdddd6fb
pushpakdpandit-stack <pushpakdpandit@gmail.com>
2025-09-28 09:59:02 +0000

    first commit

--- tourism_project/deployment/app.py
+++ tourism_project/deployment/app.py
@@ -2,7 +2,7 @@
 import streamlit as st
 import pandas as pd
 import joblib
-
+from huggingface_hub import hf_hub_download
 
 # Download and load the model
 model_path = hf_hub_download(repo_id="Pushpak21/tourism-package-model", filename="best_tourism_package_model.joblib")
